9ca53a139b8a280014e6e0116c8f9aa11d5a5071
Dmytrenko1990 <dmitrenkoartem777@gmail.com>
2023-04-02 20:23:28 -0500

    main method have been added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Car car=new Car();
+        car.brand = "BMW";
+        car.model = "X5";
+        car.year = 2022;
+    }
+}
